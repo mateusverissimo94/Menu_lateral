c597f09c5b7254e37712a313aaf3ae3fa903a36b
Mateus Barros <mateusverissimo94@gmail.com>
2021-10-29 00:10:17 -0300

    Projeto sidebar completo

---
+++ script.js
@@ -0,0 +1,15 @@
+let arrow = document.querySelectorAll('.arrow')
+for (let i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener('click', evento => {
+    console.log(evento)
+    let arrowParent = evento.target.parentElement.parentElement
+    arrowParent.classList.toggle('showMenu')
+  })
+}
+
+let sidebar = document.querySelector('.sidebar')
+let sidebarBtn = document.querySelector('.bx-menu')
+console.log(sidebarBtn)
+sidebarBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('close')
+})
